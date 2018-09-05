28f9fa8a489cd5755b45b852390d2c9390f8d9b7
不蠢会死的某翠 <shadowspacex@163.com>
2018-09-06 00:27:10 +0800

    :fire: change versionCode and modify codes

--- app/src/main/java/com/idealclover/njuclassschedule/http/HttpUtils.java
+++ app/src/main/java/com/idealclover/njuclassschedule/http/HttpUtils.java
@@ -30,7 +30,6 @@ public class HttpUtils {
     private HttpUtils() {
     }
 
-
     private static class Holder {
         private static final HttpUtils HTTP_UTILS = new HttpUtils();
     }
@@ -73,57 +72,10 @@ public class HttpUtils {
                 });
     }
 
-//    public void loadCaptcha(final File dir, final String schoolUrl, final HttpCallback<Bitmap> callback) {
-//        OkHttpUtils.post().url(schoolUrl + "/" +
-//                (schoolUrl.contains(Url.default2) ? "" : Url.default2))
-//                .build().execute(new StringCallback() {
-//            @Override
-//            public void onError(Call call, Exception e, int id) {
-//                callback.onFail(ACCESS_ERR);
-//            }
-//
-//            @Override
-//            public void onResponse(String response, int id) {
-//                Url.VIEWSTATE_LOGIN_CODE = ParseCourse.parseViewStateCode(response);
-//                LogUtil.d(this, "login_code:" + Url.VIEWSTATE_LOGIN_CODE);
-//                toLoadCaptcha(dir, schoolUrl, callback);
-//            }
-//        });
-//    }
-//
-//    private void toLoadCaptcha(final File dir, String schoolUrl, final HttpCallback<Bitmap> callback) {
-//        OkHttpUtils.get().url(schoolUrl + "/" + Url.CheckCode).build().execute(
-//                new FileCallBack(dir.getAbsolutePath(), "loadCaptcha.jpg") {
-//                    @Override
-//                    public void onError(Call call, Exception e, int id) {
-//                        e.printStackTrace();
-//                        callback.onFail(ACCESS_ERR);
-//                    }
-//
-//                    @Override
-//                    public void onResponse(File response, int id) {
-//                        Bitmap bitmap = BitmapFactory.decodeFile(
-//                                dir.getAbsolutePath() + "/loadCaptcha.jpg");
-//                        callback.onSuccess(bitmap);
-//                    }
-//                });
-//    }
-
     public void login(final String schoolUrl, final String xh, String passwd, String catpcha,
                       final String courseTime, final String term,
                       final HttpCallback<String> callback) {
         OkHttpUtils.post().url(schoolUrl + Url.LoginUrl)
-//                (schoolUrl.contains(Url.default2) ? "" : Url.default2))
-//                .addParams("__VIEWSTATE", Url.VIEWSTATE_LOGIN_CODE)
-//                .addParams("txtUserName", xh)
-//                .addParams("Textbox1", "")
-//                .addParams("Textbox2", passwd)
-//                .addParams("txtSecretCode", catpcha)
-//                .addParams("RadioButtonList1", "学生")
-//                .addParams("Button1", "")
-//                .addParams("lbLanguage", "")
-//                .addParams("hidPdrs	", "")
-//                .addParams("hidsc", "")
                 .addParams("userName", xh)
                 .addParams("password", passwd)
                 .addParams("ValidateCode", catpcha)
@@ -145,11 +97,7 @@ public class HttpUtils {
                 } else if (response.contains(app.mContext.getString(R.string.timeout_err))) {
                     callback.onFail(app.mContext.getString(R.string.timeout_err));
                 } else {
-//                    if (TextUtils.isEmpty(courseTime) || TextUtils.isEmpty(term)) {
                     toImpt(xh, schoolUrl, callback);
-//                        return;
-//                    }
-//                    toImpt(xh, schoolUrl, courseTime, term, callback);
                 }
             }
         });
@@ -164,9 +112,6 @@ public class HttpUtils {
     public void toImpt(String xh, String schoolUrl, final HttpCallback<String> callback) {
         LogUtil.d(this, "get normal+" + xh);
         OkHttpUtils.get().url(schoolUrl + Url.ClassInfo)
-//        OkHttpUtils.get().url(schoolUrl + Url.xskbcx)
-//                .addHeader("Referer", schoolUrl + "/" + Url.xskbcx + "?xh=" + xh)
-//                .addParams(Url.PARAM_XH, xh)
                 .build()
                 .execute(new StringCallback() {
                     @Override
@@ -177,58 +122,11 @@ public class HttpUtils {
 
                     @Override
                     public void onResponse(String response, int id) {
-//                        System.out.println(response);
                         Url.VIEWSTATE_POST_CODE = ParseCourse.parseViewStateCode(response);
                         callback.onSuccess(response);
                     }
                 });
     }
-
-//    /**
-//     * @param xh
-//     * @param courseTime 格式: 2017-2018
-//     * @param term       学期num
-//     * @param callback
-//     */
-//    public void toImpt(String schoolUrl, String xh, String courseTime, String term,
-//                       final HttpCallback<String> callback) {
-//        LogUtil.d("toImpt", "xh" + xh + "c" + courseTime + "t" + term);
-//        OkHttpUtils.post().url(schoolUrl + "/" + Url.xskbcx + "?xh=" + xh + "&xm=%D1%EE%D3%D1%C1%D6&gnmkdm=N121603")
-//                .addHeader("Referer", schoolUrl + "/" + Url.xskbcx + "?xh=" + xh + "&xm=%D1%EE%D3%D1%C1%D6&gnmkdm=N121603")
-//                .addHeader("Connection", "keep-alive")
-
-//                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-//                .addHeader("Accept-Encoding", "gzip, deflate")
-//                .addHeader("Accept-Language", "zh-CN,zh;q=0.9")
-//                .addHeader("Cache-Control", "max-age=0")
-//                .addHeader("Connection", "keep-alive")
-//                .addHeader("Content-Length", "5443")
-//                .addHeader("Content-Type", "application/x-www-form-urlencoded")
-//                .addHeader("Host", "210.40.2.253:8888")
-//                .addHeader("Origin", "http://210.40.2.253:8888")
-//                .addHeader("Upgrade-Insecure-Requests", "1")
-//                .addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36")
-
-//                .addParams("__EVENTTARGET", "xnd")
-//                .addParams("__EVENTARGUMENT", "")
-//                .addParams("__VIEWSTATE", Url.VIEWSTATE_POST_CODE)
-//                .addParams(Url.PARAM_XND, courseTime)
-//                .addParams(Url.PARAM_XQD, term)
-//                .build()
-//                .execute(new StringCallback() {
-//                    @Override
-//                    public void onError(Call call, Exception e, int id) {
-//                        e.printStackTrace();
-//                        callback.onFail(ACCESS_ERR);
-//                    }
-//
-//                    @Override
-//                    public void onResponse(String response, int id) {
-//                        Url.VIEWSTATE_POST_CODE = ParseCourse.parseViewStateCode(response);
-//                        callback.onSuccess(response);
-//                    }
-//                });
-//    }
 
     /**
      * 上传解析失败的课表html
@@ -240,5 +138,4 @@ public class HttpUtils {
         String newTag = LogUtil.getNewTag(tag);
 
     }
-
 }
