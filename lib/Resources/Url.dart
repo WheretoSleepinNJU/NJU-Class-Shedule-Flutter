bcca06687fe8b7d7a99f7381bec0639ee714cfce
idealclover <idealclover@163.com>
2019-09-06 19:05:30 +0800

    :bug: fixed url bug in Alipay

--- lib/Resources/Url.dart
+++ lib/Resources/Url.dart
@@ -3,7 +3,8 @@ class Url {
   static final String ClassInfo = "student/teachinginfo/courseList.do?method=currentTermCourse";
   static final String LoginUrl = "login.do";
 
-  static final String ALIPAY_URL = 'alipayqr://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https://qr.alipay.com/FKX00710CQCHIHK4B9CA31';
+//  static final String ALIPAY_URL = 'alipayqr://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https://qr.alipay.com/FKX00710CQCHIHK4B9CA31';
+  static final String ALIPAY_URL = 'alipays://platformapi/startapp?appId=10000007&qrcode=https://qr.alipay.com/FKX00710CQCHIHK4B9CA31';
   static final String QQ_GROUP_APPLE_URL = 'https://jq.qq.com/?_wv=1027&k=5hL1ccO';
   static final String QQ_GROUP_ANDROID_URL = 'https://jq.qq.com/?_wv=1027&k=5PNwtkh';
 }
