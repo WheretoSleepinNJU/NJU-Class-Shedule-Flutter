8f2989dada6b7109d9c8133c5276c242e163a00f
idealclover <idealclover@live.com>
2020-02-10 18:41:16 +0800

    :bug: fix bugs

--- lib/Resources/Config.dart
+++ lib/Resources/Config.dart
@@ -1,9 +1,9 @@
 class Config{
   static final int MAX_CLASSES = 13;
-  static final int MAX_WEEKS = 20;
+  static final int MAX_WEEKS = 30;
   static final int DEFAULT_WEEK_NUM = 17;
 
-  static final String SEMESTER_START_MONDAY = '2019-09-02';
+  static final String SEMESTER_START_MONDAY = '2020-02-10';
 
   static final String default_class_table = "默认课表";
 
